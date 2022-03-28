31ba5bffcb2a30ed92f2fd98e72a1e0fbeb62d89
phongteaa <phongteaa@gmail.com>
2022-03-28 14:15:20 +0700

    xxx

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,13 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        MountainBicycle mb = new MountainBicycle(3, 100, 40);
+        MountainBicycle mb = new MountainBicycle(3, 80, 40);
         System.out.println("\nMountain bicycle ------>");
-        System.out.println(mb.toString());
+        System.out.println(mb);
 
-        KidBicycle kb = new KidBicycle(1, 50, 3);
+        KidBicycle kb = new KidBicycle(1, 20, 3);
         System.out.println("\nKid bicycle ------>");
-        System.out.println(kb.toString());
+        System.out.println(kb);
     }
 }
 
